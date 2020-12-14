a540cf5e6683d19e46eedcff0d714bebed31f3e2
Abhishek Sohagiya <abhishek.s@zymrs-MacBook-Pro.local>
2020-12-14 16:59:47 +0530

    Updated status bar

--- src/ios/CDVWKInAppBrowser.m
+++ src/ios/CDVWKInAppBrowser.m
@@ -34,7 +34,7 @@
 
 #define    IAB_BRIDGE_NAME @"cordova_iab"
 
-#define    TOOLBAR_HEIGHT 0.0
+#define    TOOLBAR_HEIGHT 44.0
 #define    LOCATIONBAR_HEIGHT 0.0
 #define    FOOTER_HEIGHT ((TOOLBAR_HEIGHT) + (LOCATIONBAR_HEIGHT))
 
@@ -1132,7 +1132,7 @@ BOOL isExiting = FALSE;
 
 - (void) rePositionViews {
     CGRect viewBounds = [self.webView bounds];
-    CGFloat statusBarHeight = [self getStatusBarOffset];
+    CGFloat statusBarHeight = 0.0;
     
     // orientation portrait or portraitUpsideDown: status bar is on the top and web view is to be aligned to the bottom of the status bar
     // orientation landscapeLeft or landscapeRight: status bar height is 0 in but lets account for it in case things ever change in the future
